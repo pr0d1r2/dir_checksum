707cebf7641353936089a85768e39c60c7f3b686
Marcin Nowicki <pr0d1r2@gmail.com>
2014-04-20 07:58:45 +0200

    Add script itself.

---
+++ dir_checksum.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'digest/md5'
+require 'fileutils'
+
+start_dir = Dir.pwd
+
+ARGV.each do |dir|
+  checksum_file = "#{dir}.dir_checksum.yml"
+  checksum_file_ok = "#{dir}.dir_checksum.ok"
+  if File.file?(checksum_file)
+    if File.directory?(dir)
+      if File.file?(checksum_file_ok)
+        puts "#{dir} : [ALL OK confirmed before]"
+        exit 0
+      else
+        dir_data = YAML.load_file(checksum_file)
+        Dir.chdir(dir)
+        dir_data.each do |file,data|
+          if File.size(file) == data["size"]
+            puts "#{file} [SIZE OK]"
+          else
+            puts "#{file} [SIZE BAD]"
+            exit 1
+          end
+        end
+        dir_data.each do |file,data|
+          if Digest::MD5.hexdigest(File.read(file)) == data["md5sum"]
+            puts "#{file} [md5sum OK]"
+          else
+            puts "#{file} [md5sum BAD]"
+            exit 2
+          end
+        end
+        Dir.chdir('..')
+        FileUtils.touch(checksum_file_ok)
+        Dir.chdir(start_dir)
+      end
+    end
+  else
+    if File.directory?(dir)
+      Dir.chdir(dir)
+      dir_data = Hash[
+        Dir.glob("**/*").map do |file|
+          puts file
+          if File.file?(file)
+            [
+              file, {
+                "size" => File.size(file),
+                "md5sum" => Digest::MD5.hexdigest(File.read(file))
+              }
+            ]
+          end
+        end
+      ]
+    else
+      raise "There is no directory: #{dir}"
+    end
+    Dir.chdir(start_dir)
+    File.open("#{dir}.dir_checksum.yml", 'w+') { |f| f.write(dir_data.to_yaml) }
+  end
+end
